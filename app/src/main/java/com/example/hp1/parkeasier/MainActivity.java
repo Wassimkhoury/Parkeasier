a7fea796d0c57359813bd0d765e9070b04265f7d
wassimkhoury <wassimkh@hotmail.com>
2017-09-28 12:41:08 +0300

    aa

--- app/src/main/java/com/example/hp1/parkeasier/MainActivity.java
+++ app/src/main/java/com/example/hp1/parkeasier/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hp1.parkeasier;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,22 +10,12 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener{
-    Button BT1;
-    Button BT3;
-    Button BT4;
-    Button BT5;
-    Button BT6;
-    Button BT7;
-    Button BT8;
-    Button BT9;
-    Button BT10;
-    Button BT11;
-    Button BT12;
-    Button BT13;
-    Button BT14;
-    Button BT15;
-    Button BT16;
+import static android.R.attr.button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button[] btn = new Button[16];
+    int[] ids = {R.id.BT1, R.id.BT2, R.id.BT3, R.id.BT4, R.id.BT5, R.id.BT6, R.id.BT7, R.id.BT8, R.id.BT9, R.id.BT10, R.id.BT11, R.id.BT12, R.id.BT13, R.id.BT14, R.id.BT15, R.id.BT16};
 
     Spinner spCoffee1;
     Spinner spCoffee2;
@@ -33,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     Adapter adapter3;
     Adapter adapter2;
     Adapter adapter1;
+
     String[] spList ={"None","disablity","pregnant"};
     String[] spList2 ={"None","Gym","shopping",};
     String[] spList3 ={"floor1","floor2"};
@@ -42,30 +34,46 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        for(int i=0;i<btn.length;i++){
+            btn[i] = (Button) findViewById(ids[i]);
+            btn[i].setOnClickListener(this);
+        }
+
         spCoffee1=(Spinner) findViewById(R.id.spCoffee1);
-        spCoffee2=(Spinner) findViewById(R.id.spCoffee2);
-        spCoffee3=(Spinner) findViewById(R.id.spCoffee3);
         ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spList);
         spCoffee1.setAdapter(adapter1);
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spList);
+        spCoffee1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        spCoffee2=(Spinner) findViewById(R.id.spCoffee2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spList2);
         spCoffee2.setAdapter(adapter2);
-        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spList);
+        spCoffee2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        spCoffee3=(Spinner) findViewById(R.id.spCoffee3);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spList3);
         spCoffee3.setAdapter(adapter3);
+        spCoffee3.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-
-
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
-        if(BT1==v){
-            BT1.setBackgroundColor();
+        for(int i=0;i<btn.length;i++){
+            btn[i].setBackgroundColor(Color.RED);
         }
-
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
     }
 }
---
+++ app/src/main/java/com/example/hp1/parkeasier/customAdapter.java
@@ -0,0 +1,50 @@
+package com.example.hp1.parkeasier;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.hp1.parkeasier.R;
+
+import java.util.List;
+
+/**
+ * Created by Hp1 on 27/09/2017.
+ */
+
+class CustomAdapter extends ArrayAdapter<Item> {
+
+    private int resource;
+
+    public CustomAdapter(Context context, int resource, List<Item> objects) {
+        super(context, resource, objects);
+        this.resource = resource;
+    }
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LayoutInflater imageInflater = LayoutInflater.from(getContext());
+        View cuView = imageInflater.inflate(resource, parent, false );
+
+        Item item = getItem(position);
+        TextView title = (TextView) cuView.findViewById(R.id.textView);
+        ImageView image = (ImageView) cuView.findViewById(R.id.imageView);
+        Button btnClick = (Button) cuView.findViewById(R.id.button);
+
+        title.setText(item.getTitle());
+        image.setImageResource(item.getImageId());
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"Item Poisition: "+position,Toast.LENGTH_LONG).show();
+            }
+        });
+        return cuView;
+    }
+}
